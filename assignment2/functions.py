3f806022ef43c44980094b70d359f57454ea9e86
Jacob <jacmalm@kth.se>
2022-04-08 11:58:43 +0200

    forward pass

--- assignment2/functions.py
+++ assignment2/functions.py
@@ -31,3 +31,16 @@ def combine_train_sets():
         train_Y = np.concatenate((train_Y, more_Y), axis=1)
         train_y = np.concatenate((train_y, more_y))
     return train_X, train_Y, train_y
+
+
+def forward_pass(X, W_1, b_1, W_2, b_2):
+    s_1 = W_1 @ X + b_1
+    h_1 = np.maximum(0, s_1)
+    s_2 = W_2 @ h_1 + b_2
+    p = softmax(s_2)
+    return h_1, p
+
+
+def softmax(x):
+    """ Standard definition of the softmax function """
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
