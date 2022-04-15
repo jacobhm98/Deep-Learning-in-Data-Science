659cc5591fad53091c00cf38052c9b815a75a7d7
Jacob <jacmalm@kth.se>
2022-04-15 17:06:03 +0200

    all data collected

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -17,7 +17,8 @@ def task1_2():
     W, b = initialize_network_params(60, train_X.shape[0], train_Y.shape[0])
     etas = [1e-5, 1e-1, 800]
     GDParams = [100, etas, 3]
-    W, b, train_cost, val_cost, train_accuracy, val_accuracy = MiniBatchGD([train_X, train_Y, train_y], [val_X, val_Y, val_y], GDParams, W, b, 0.01)
+    W, b, train_cost, val_cost, train_accuracy, val_accuracy = MiniBatchGD([train_X, train_Y, train_y],
+                                                                           [val_X, val_Y, val_y], GDParams, W, b, 0.01)
     plot_train_and_val_performance(train_cost, val_cost, 'Cost')
     plot_train_and_val_performance(train_accuracy, val_accuracy, 'Accuracy')
     print(train_cost)
@@ -52,7 +53,7 @@ def fine_search():
     test_X = test_X[:, 1000:]
     test_Y = test_Y[:, 1000:]
     test_y = test_y[1000:]
-    performance_of_lambdas = search_range_of_lambda(-6, -3, 10, [train_X, train_Y, train_y], [val_X, val_Y, val_y])
+    performance_of_lambdas = search_range_of_lambda(-5, -3, 30, [train_X, train_Y, train_y], [val_X, val_Y, val_y])
     print(performance_of_lambdas)
     items = performance_of_lambdas.items()
     items = sorted(items)
@@ -61,9 +62,31 @@ def fine_search():
     plt.show()
 
 
+def best_param_performance():
+    best_lambda = 0.009
+    train_X, train_Y, train_y = combine_train_sets()
+    test_X, test_Y, test_y = unpack_batch(LoadBatch('test_batch'))
+    val_X = test_X[:, :1000]
+    val_Y = test_Y[:, :1000]
+    val_y = test_y[:1000]
+    test_X = test_X[:, 1000:]
+    test_Y = test_Y[:, 1000:]
+    test_y = test_y[1000:]
+    W, b = initialize_network_params(60, train_X.shape[0], train_Y.shape[0])
+    etas = [1e-5, 1e-1, 2 * floor(train_X.shape[1] / 100)]
+    GDParams = [100, etas, 5]
+    W, b, train_cost, val_cost, train_accuracy, val_accuracy = MiniBatchGD([train_X, train_Y, train_y],
+                                                                           [val_X, val_Y, val_y], GDParams, W, b,
+                                                                           best_lambda)
+    plot_train_and_val_performance(train_cost, val_cost, 'Cost')
+    plot_train_and_val_performance(train_accuracy, val_accuracy, 'Accuracy')
+    print(train_cost)
+    print(val_cost)
+    print(ComputeAccuracy(test_X, test_y, W, b))
+
 
 def main():
-    fine_search()
+    best_param_performance()
 
 
 main()
