6cea83b824e2b3f4f90513a36792a32f9261e77f
Jacob <jacmalm@kth.se>
2022-06-09 21:39:02 +0200

    think that shit works it runs at least

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -2,8 +2,21 @@ import functions
 
 
 def main():
+    sanity_check_gradients()
+
+
+def sanity_check_gradients():
     book_data, book_chars = functions.read_in_text('goblet_book.txt')
     functions.K = len(book_chars)
-
+    char_to_int, int_to_char = functions.get_maps(book_chars)
+    X = book_data[:functions.seq_length]
+    Y = book_data[1:functions.seq_length + 1]
+    X = functions.convert_char_list_to_ints(X, char_to_int)
+    Y = functions.convert_char_list_to_ints(Y, char_to_int)
+    X = functions.one_hotify(X)
+    Y = functions.one_hotify(Y)
+    RNN = functions.initialize_parameters()
+    activations, loss = functions.forward_pass(RNN, X, Y)
+    del_RNN = functions.back_pass(RNN, activations, Y)
 
 main()
--- assignment4/functions.py
+++ assignment4/functions.py
@@ -10,6 +10,12 @@ seq_length = 25
 K = None
 
 
+def convert_char_list_to_ints(chars, chars_to_int):
+    for i, char in enumerate(chars):
+        chars[i] = chars_to_int[char]
+    return chars
+
+
 def read_in_text(file_name):
     with open(file_name, 'r') as f:
         book_data = [char for char in f.read()]
@@ -22,24 +28,32 @@ def get_maps(unique_list):
     for idx, char in enumerate(unique_list):
         idx_to_char[idx] = char
         char_to_idx[char] = idx
+    return char_to_idx, idx_to_char
 
 
 def synthesize_text(RNN, h0, x0, n):
     outputs = []
     for t in range(n):
-        p_t, h_t = single_forward_pass(RNN, h0, x0)
+        _, h_t, p_t = single_forward_pass(RNN, h0, x0)
         cp = np.cumsum(p_t)
         a = random.uniform(0, 1)
         indices = np.argwhere(cp - a)
         k = indices[0]
-        x_next = one_hot(k, K)
+        x_next = one_hot(k)
         outputs.append(x_next)
         h0 = h_t
         x0 = x_next
     return np.array(outputs)
 
 
-def one_hot(value, K):
+def one_hotify(vec):
+    mat = np.zeros((len(vec), K))
+    for i, val in enumerate(vec):
+        mat[i] = one_hot(val)
+    return mat.T
+
+
+def one_hot(value):
     hot = np.zeros(K)
     hot[value] = 1
     return hot
@@ -51,39 +65,60 @@ def int_from_one_hot(vec):
 
 
 def ComputeCost(Y, predictions):
-    ce_term = - np.log((Y * predictions).sum(axis=0)).mean()
+    ce_term = - np.log(Y.T @ predictions)
     return ce_term
 
 
+def back_pass(RNN, activations, labels, h_0=np.zeros(m)):
+    # activations[t][0] is a_t, activations[t][1] is h_t, activations[t][2] is x_t
+    # Initialize ds thats gonna hold my grads
+    del_RNN = {}
+    del_RNN['b'] = np.zeros((m, 1))
+    del_RNN['c'] = np.zeros((K, 1))
+    del_RNN['U'] = np.zeros((m, K))
+    del_RNN['W'] = np.zeros((m, m))
+    del_RNN['V'] = np.zeros((K, m))
+    T = len(activations)
+    del_h = None
+    del_a = None
+    for t in range(T - 1, -1, -1):
+        G = - (labels[:, t].reshape((-1, 1)) - activations[t][2]).T
+        del_RNN['V'] = del_RNN['V'] + G.T @ activations[t][1].T
+        del_RNN['c'] = del_RNN['c'] + G.T
+        if t == T - 1:
+            del_h = G @ RNN['V']
+        else:
+            del_h = G @ RNN['V'] + del_a @ RNN['W']
+        del_a = del_h @ np.diag(1 - np.power(np.tanh(activations[t][0].flatten()), 2))
+        if t == 0:
+            del_RNN['W'] = del_RNN['W'] + del_a.T @ h_0.T
+        else:
+            del_RNN['W'] = del_RNN['W'] + del_a.T @ activations[t - 1][1].T
+        del_RNN['U'] = del_RNN['U'] + del_a.T @ activations[t][2].T
+        del_RNN['b'] = del_RNN['b'] + del_a.T
+        return del_RNN
+
+
 def forward_pass(RNN, X_chars, Y_chars):
-    h = np.zeros(m)
+    h = np.zeros((m, 1))
     cost = 0
     activations = []
-    for char, label in zip(X_chars, Y_chars):
-        predictions, h = single_forward_pass(RNN, h, char)
-        cost += ComputeCost(label, predictions)
-        activations.append((deepcopy(h), predictions))
+    n = X_chars.shape[1]
+    for i in range(n):
+        a, h, predictions = single_forward_pass(RNN, h, X_chars[:, i])
+        cost += ComputeCost(Y_chars[:, i], predictions)
+        activations.append((a, deepcopy(h), predictions))
     return activations, cost
 
 
-
-def back_pass(RNN, activations, cost):
-    # Initialize ds thats gonna hold my grads
-    delta_RNN = {}
-    delta_RNN['b'] = np.zeros((m, 1))
-    delta_RNN['c'] = np.zeros((K, 1))
-    delta_RNN['U'] = np.zeros((m, K))
-    delta_RNN['W'] = np.zeros((m, m))
-    delta_RNN['V'] = np.zeros((K, m))
-
-
-
 def single_forward_pass(RNN, h0, x0):
+    h0 = h0.reshape((-1, 1))
+    x0 = x0.reshape((-1, 1))
     a = RNN['W'] @ h0 + RNN['U'] @ x0 + RNN['b']
     h1 = np.tanh(a)
     o = RNN['V'] @ h1 + RNN['c']
     p = softmax(o)
-    return p, h1
+    return a, h1, p
 
 
 def softmax(x):
@@ -91,7 +126,7 @@ def softmax(x):
     return e_x / e_x.sum()
 
 
-def initialize_parameters(m, K):
+def initialize_parameters():
     RNN = {}
     RNN['b'] = np.zeros((m, 1))
     RNN['c'] = np.zeros((K, 1))
